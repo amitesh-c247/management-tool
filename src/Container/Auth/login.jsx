9c8e0de5e5c52afae1a39918843e39c8123e45bf
Amitesh <amitesh.p@chapter247.com>
2021-05-17 18:22:55 +0530

    changes in auth code

--- src/Container/Auth/login.jsx
+++ src/Container/Auth/login.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import { Card, Row, Col, Container, Form, Button } from 'react-bootstrap';
+import React from "react";
+import { Card, Row, Col, Container, Form, Button } from "react-bootstrap";
 import { connect } from "react-redux";
-import { loginRequest } from "../../Redux/Action"
+import { loginRequest } from "../../Redux/Action";
+import loginFormSchema from "../../Helper/ValidSchema";
 
 class Login extends React.Component {
   constructor(props) {
@@ -9,37 +10,73 @@ class Login extends React.Component {
     this.state = {
       email: "",
       password: "",
-      token: ''
-    }
+      token: "",
+      errors: {
+        email: "",
+        password: "",
+      },
+    };
   }
 
   componentDidMount = () => {
-    const data = localStorage.getItem("token")
-    if (data ) {
-      this.props.history.push('/dashboard')
+    const data = localStorage.getItem("token");
+    if (data) {
+      this.props.history.push("/dashboard");
     }
-  }
+  };
 
   handleChange = (e) => {
     this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+      [e.target.name]: e.target.value,
+    });
+  };
 
   submitForm = (e) => {
     e.preventDefault();
     const { email, password } = this.state;
+    let errors = {
+      email: "",
+      password: "",
+    };
+    loginFormSchema
+      .validate(
+        {
+          email,
+          password,
+        },
+        { abortEarly: false }
+      )
+      .then(
+        this.setState({
+          email,
+          password,
+        }),
+        this.props.loginRequest({
+          email: email,
+          password: password,
+        }),
+        this.setState({
+          errors: "",
+        })
+      )
+      .catch((err) => {
+        err.inner.forEach((error) => {
+          errors = { ...errors, [error.path]: error.message };
+        });
+        this.setState({
+          errors,
+        });
+      });
 
-    this.setState({
-      email,
-      password,
-    })
-    this.props.loginRequest({ email: email, password: password })
-
-  }
+    // this.setState({
+    //   email,
+    //   password,
+    // })
+    // this.props.loginRequest({ email: email, password: password })
+  };
 
   render() {
-    const { email, password } = this.state;
+    const { email, password, errors } = this.state;
     return (
       <>
         <Container>
@@ -60,6 +97,7 @@ class Login extends React.Component {
                           placeholder="Enter Email"
                           onChange={this.handleChange}
                         />
+                        <p className="text-danger">{errors.email}</p>
                       </Form.Group>
                       <Form.Group controlId="formBasicEmail">
                         <Form.Label>Password</Form.Label>
@@ -70,8 +108,13 @@ class Login extends React.Component {
                           placeholder="Enter password"
                           onChange={this.handleChange}
                         />
+                        <p className="text-danger">{errors.password}</p>
                       </Form.Group>
-                      <Button variant="primary" type="submit" onClick={this.submitForm}>
+                      <Button
+                        variant="primary"
+                        type="submit"
+                        onClick={this.submitForm}
+                      >
                         Submit
                       </Button>
                     </Form>
@@ -86,15 +129,12 @@ class Login extends React.Component {
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = dispatch => ({
-  loginRequest: data => {
+const mapDispatchToProps = (dispatch) => ({
+  loginRequest: (data) => {
     dispatch(loginRequest(data));
   },
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Login);
+export default connect(mapStateToProps, mapDispatchToProps)(Login);
