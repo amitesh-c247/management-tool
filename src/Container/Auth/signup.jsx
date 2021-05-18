a79d8f61aad340d75fb3f6a495fef27c4e03674e
Amitesh <amitesh.p@chapter247.com>
2021-05-18 19:02:38 +0530

    work on signup page

--- src/Container/Auth/signup.jsx
+++ src/Container/Auth/signup.jsx
@@ -1,144 +1,142 @@
-import React from 'react';
-import { Card, Row, Col, Container, Form, Button } from 'react-bootstrap';
+import React, { useState, useEffect } from "react";
+import { Card, Row, Col, Container, Form, Button } from "react-bootstrap";
 import { connect } from "react-redux";
-import { signupRequest } from "../../Redux/Action"
-import loginFormSchema from "../../Helper/ValidSchema"
+import { signupRequest } from "../../Redux/Action";
+import loginFormSchema from "../../Helper/ValidSchema";
+const Signup = (props) => {
+  const initialStates = {
+    email: "",
+    password: "",
+    user_name: "",
+    token: "",
+  };
+  const errorsState = {
+    email: "",
+    password: "",
+    user_name: "",
+  };
+  const [inputs, setInputs] = useState(initialStates);
+  const [errors, setError] = useState(errorsState);
 
-class Signup extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      email: "",
-      password: "",
-      user_name:'',
-      token: '',
-      errors:{
-        email:'',
-        password:'',
-        user_name:'',
-      },
-    }
-  }
-
-  componentDidMount = () => {
-    const data = localStorage.getItem("token")
+  useEffect(() => {
+    const data = localStorage.getItem("token");
     if (data) {
-      this.props.history.push('/dashboard')
+      props.history.push("/dashboard");
     }
-  }
+  }, []);
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.name]: e.target.value
-    })
-  }
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+    setError({
+      ...errors,
+      [e.target.name]: "",
+    });
+  };
 
-  submitForm = (e) => {
+  const signupSubmit = (e) => {
     e.preventDefault();
-    const { user_name, email, password } = this.state;
-    let errors = {
-      user_name:'',
-      email: '',
-      password: '',
-    }
-    loginFormSchema.validate  ({
-      user_name, email, password
-    },{ abortEarly: false })
+    loginFormSchema
+      .validate(
+        {
+          email: inputs.email,
+          password: inputs.password,
+          user_name: inputs.user_name,
+        },
+        { abortEarly: false }
+      )
       .then(
-        this.setState({
-          user_name,
-          email,
-          password,
-        }),
-        this.props.signupRequest({ 
-          user_name:user_name, 
-          email: email, 
-          password: password 
-        }),
-        this.setState({
-          errors:''
+        props.signupRequest({
+          email: inputs.email,
+          password: inputs.password,
+          user_name: inputs.user_name,
         })
-      ).catch(err => {
-        err.inner.forEach(error => {
-          errors = { ...errors, [error.path]: error.message };
+        // console.log(inputs, "=============")
+      )
+      .catch((err) => {
+        let tempErrors = {};
+        err.inner.forEach((error) => {
+          tempErrors = {
+            ...tempErrors,
+            [error.path]: error.message,
+          };
         });
-        this.setState({
-          errors
-        })
+        setError({
+          ...errors,
+          ...tempErrors,
+        });
       });
-  }
+  };
 
-  render() {
-    const { email, password ,user_name,errors } = this.state;
-  
-    return (
-      <>
-        <Container>
-          <div className={"about-warp mb-5"}>
-            <h1 className={"text-center pt-4 pb-4"}>Sign Up</h1>
-            <Row className={"justify-content-center"}>
-              <Col md={6} sm={6}>
-                <Card>
-                  <Card.Body>
-                    {/* <Card.Title className={"text-center"}>{aboutBlock.firstBlock.title}</Card.Title> */}
-                    <Form>
-                      <Form.Group controlId="formBasicUserName">
-                        <Form.Label>User Name</Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="user_name"
-                          value={user_name}
-                          placeholder="Enter Username"
-                          onChange={this.handleChange}
-                        />
-                      </Form.Group>
+  return (
+    <>
+      <Container>
+        <div className={"about-warp mb-5"}>
+          <h1 className={"text-center pt-4 pb-4"}>Login</h1>
+          <Row className={"justify-content-center"}>
+            <Col md={6} sm={6}>
+              <Card>
+                <Card.Body>
+                  <Form onSubmit={signupSubmit}>
+                    <Form.Group controlId="formBasicUserName">
+                      <Form.Label>User Name</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="user_name"
+                        value={inputs.user_name || ""}
+                        placeholder="Enter Username"
+                        onChange={handleChange}
+                      />
                       <p className="text-danger">{errors.user_name}</p>
-                      <Form.Group controlId="formBasicPassword">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="email"
-                          value={email}
-                          placeholder="Enter Email"
-                          onChange={this.handleChange}
-                        />
-                      </Form.Group>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Email address</Form.Label>
+                      <Form.Control
+                        type="email"
+                        value={inputs.email || ""}
+                        placeholder="Enter email"
+                        name="email"
+                        onChange={handleChange}
+                      />
                       <p className="text-danger">{errors.email}</p>
-                      <Form.Group controlId="formBasicEmail">
-                        <Form.Label>Password</Form.Label>
-                        <Form.Control
-                          type="text"
-                          name="password"
-                          value={password}
-                          placeholder="Enter password"
-                          onChange={this.handleChange}
-                        />
-                      </Form.Group>
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                      <Form.Label>Password</Form.Label>
+                      <Form.Control
+                        type="text"
+                        name="password"
+                        value={inputs.password || ""}
+                        placeholder="Enter password"
+                        onChange={handleChange}
+                      />
                       <p className="text-danger">{errors.password}</p>
-                      <Button variant="primary" type="submit" onClick={this.submitForm}>
-                        Submit
-                      </Button>
-                    </Form>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-          </div>
-        </Container>
-      </>
-    );
-  }
-}
+                    </Form.Group>
+                    <Button
+                      variant="primary"
+                      type="submit"
+                      onClick={signupSubmit}
+                    >
+                      Submit
+                    </Button>
+                  </Form>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </div>
+      </Container>
+    </>
+  );
+};
 
-const mapStateToProps = state => ({});
+const mapStateToProps = (state) => ({});
 
-const mapDispatchToProps = dispatch => ({
-  signupRequest: data => {
+const mapDispatchToProps = (dispatch) => ({
+  signupRequest: (data) => {
     dispatch(signupRequest(data));
   },
 });
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Signup);
+export default connect(mapStateToProps, mapDispatchToProps)(Signup);
